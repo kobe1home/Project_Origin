9352fae842156e2359b6961d67cfdd55020cccf9
pengre <pengare@gmail.com>
2012-02-09 20:16:45 -0500

    1.Refactor waypoint to Path 2.Add FPS to handle performance benchmark

--- Project_Origin/Project_Origin/Map.cs
+++ Project_Origin/Project_Origin/Map.cs
@@ -20,10 +20,7 @@ namespace Project_Origin
         private GraphicsDevice device;
         private Game game;
 
-        private List<WayPoint> points = new List<WayPoint>();
-        private List<VertexPositionColor> lines = new List<VertexPositionColor>();
-
-        private MouseState previousState;
+        //private MouseState previousState;
 
         private static int GridWidth = 2;
         private static Color DefaultColor = Color.Blue;
@@ -44,7 +41,7 @@ namespace Project_Origin
                 throw new InvalidOperationException("ICameraService not found.");
             }
 
-            this.previousState = Mouse.GetState();
+            //this.previousState = Mouse.GetState();
         }
 
         public override void Initialize()
@@ -80,7 +77,7 @@ namespace Project_Origin
 
         public override void Update(GameTime gameTime)
         {
-            CheckMouseClick();
+            
 
         }
 
@@ -114,99 +111,25 @@ namespace Project_Origin
                                                 VertexPositionColor.VertexDeclaration);
                 }
             }
-
-
-            this.drawAllWayPoints();
             
             //this.device.RasterizerState = prevRs;
             base.Draw(gameTime);
         }
 
-        private void CheckMouseClick()
+        //Get methods
+        public int getWitdth()
         {
-            MouseState mouseState = Mouse.GetState();
-
-            
-            if (mouseState.RightButton == ButtonState.Pressed && this.previousState.RightButton == ButtonState.Released)
-            {
-
-                Ray pickRay = GetPickRay(mouseState);
-
-                BoundingBox box = new BoundingBox(this.start - new Vector3(this.witdth / 2, this.heigh / 2, 0.0f), this.start + new Vector3(this.witdth / 2, this.heigh / 2, 0.0f));
-
-                Nullable<float> result = pickRay.Intersects(box);
-                if (result.HasValue == true)
-                {
-                    Vector3 selectedPoint = pickRay.Position + pickRay.Direction * result.Value;
-                    WayPoint waypoint = new WayPoint(this.game.GraphicsDevice, selectedPoint + new Vector3(0.0f, 0.0f, WayPoint.CubeSize / 2));
-                    this.points.Add(waypoint);
-                    this.lines.Add(new VertexPositionColor(waypoint.CenterPos, Color.White));
-                }
-            }
-            this.previousState = mouseState;
+            return this.witdth;
+        }
+        public int getHeigh()
+        {
+            return this.heigh;
+        }
+        public Vector3 getStart()
+        {
+            return this.start;
         }
 
-        private Ray GetPickRay(MouseState mouseState)
-        {
-            //MouseState mouseState = Mouse.GetState();
 
-            int mouseX = mouseState.X;
-            int mouseY = mouseState.Y;
-
-            Vector3 nearsource = new Vector3((float)mouseX, (float)mouseY, 0f);
-            Vector3 farsource = new Vector3((float)mouseX, (float)mouseY, 1f);
-
-            Matrix world = Matrix.CreateTranslation(0, 0, 0);
-
-            Vector3 nearPoint = GraphicsDevice.Viewport.Unproject(nearsource,
-                this.camera.ProjectMatrix, this.camera.ViewMatrix, world);
-
-            Vector3 farPoint = GraphicsDevice.Viewport.Unproject(farsource,
-                this.camera.ProjectMatrix, this.camera.ViewMatrix, world);
-
-            // Create a ray from the near clip plane to the far clip plane.
-            Vector3 direction = farPoint - nearPoint;
-            direction.Normalize();
-            Ray pickRay = new Ray(nearPoint, direction);
-
-            return pickRay;
-        }
-
-        private void drawAllWayPoints()
-        {
-            if (this.points.Count == 1)
-            {
-                WayPoint pointStart = this.points[0];
-                pointStart.Draw(this.camera.ViewMatrix, this.camera.ProjectMatrix, Color.Red);
-            }
-            if (this.points.Count >= 2)
-            {
-                for (int index = 0; index < this.points.Count; index++)
-                {
-                    WayPoint point = this.points[index];
-                    point.Draw(this.camera.ViewMatrix, this.camera.ProjectMatrix, Color.Red);
-                }
-                this.drawAllLines();
-            }
-        }
-
-        private void drawAllLines()
-        {
-            BasicEffect defaultEfft = new BasicEffect(this.GraphicsDevice);
-
-            defaultEfft.VertexColorEnabled = true;
-            defaultEfft.World = Matrix.Identity;
-            defaultEfft.View = this.camera.ViewMatrix;
-            defaultEfft.Projection = this.camera.ProjectMatrix;
-
-            foreach (EffectPass pass in defaultEfft.CurrentTechnique.Passes)
-            {
-                pass.Apply();
-                this.device.DrawUserPrimitives<VertexPositionColor>(PrimitiveType.LineStrip,
-                                                                    this.lines.ToArray(), 0,
-                                                                    this.lines.Count - 1);
-            }
-  
-        }
     }
 }
